d6cb954396444797cc35d465369e983dbd8b9dfb
jayasreecodejava10 <kottejay7401@gmail.com>
2025-01-09 15:51:47 -0800

    added extra new branch code

--- Proj01/src/com/nt/payment/DigitalPayment.java
+++ Proj01/src/com/nt/payment/DigitalPayment.java
@@ -5,4 +5,7 @@ public class DigitalPayment {
 	public String NetBanking(long UPID,double amnt) {
 		return amnt+"is received from this"+UPID;
 	}
+	public String CashBackOffer(long txID,double amnt) {
+		return amnt*0.05+"points are added to this"+txID+"as reward points";
+	}
 }
