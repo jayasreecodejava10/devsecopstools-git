26ec0d84faed43eac40c3d45953369d257c3ac8c
jayasreecodejava10 <kottejay7401@gmail.com>
2025-01-09 14:22:08 -0800

    modified service.java

--- Proj01/src/com/nt/service/service.java
+++ Proj01/src/com/nt/service/service.java
@@ -1,10 +1,14 @@
 package com.nt.service;
 
 public class service {
-    
+     static int x=20;
+     static int y=10;
+     
 	public int sum(int x,int y) {
-		 x=20;
-		 y=10;
+		 
 		return x+y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
 	}
 }
