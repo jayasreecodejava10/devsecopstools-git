a66c5977f2d4ab7004c4eb63497a592118db3be4
jayasreecodejava10 <kottejay7401@gmail.com>
2025-01-09 14:37:30 -0800

    commenting methods

--- Proj01/src/com/nt/service/service.java
+++ Proj01/src/com/nt/service/service.java
@@ -3,11 +3,12 @@ package com.nt.service;
 public class service {
      static int x=20;
      static int y=10;
-     
+     //Addition method
 	public int sum(int x,int y) {
 		 
 		return x+y;
 	}
+	//Subtraction method
 	public int sub(int x,int y) {
 		return x-y;
 	}
